290988142977af92a6fbd810497a13ce3f967e7c
kngan2201 <2351050111ngan@ou.edu.vn>
2024-09-03 17:39:55 +0700

    up

--- js/index.js
+++ js/index.js
@@ -19,4 +19,45 @@ window.onload = function () {
     closemenu.onclick = function () {
         sidebarMenu.classList.remove("show");
     } 
+
+//---------- TIN NOI BAT -----------------------------------------------------------//
+    let listImg = document.querySelector(".listImg")
+    let imageGT = document.querySelectorAll(".gallery .image")
+    let gallery = document.querySelector(".gallery")
+    var current = 0;
+    var w = gallery.offsetWidth;
+    console.log(w);
+    const changeImg = function() {
+        if (current == imageGT.length-1) {
+            listImg.style.transition = 'transform 0s';
+            listImg.style.transform = `translateX(${0}px)`;
+            current = 0; 
+        }
+        else {
+            current++;
+            listImg.style.transform = `translateX(${-w * current}px)`;
+        }
+    }
+    let clear = setInterval(() => { changeImg()}, 2500);
+    let prev = document.querySelector(".btnGT .prev")
+    let next = document.querySelector(".btnGT .next")
+    console.log(prev, next)
+        prev.onclick = function() {
+            clearInterval(clear);
+            if (current == 0) {
+                current = imageGT.length-1; 
+                listImg.style.transition = 'transform 0s';
+                listImg.style.transform = `translateX(${-w * current}px)`;
+            }
+            else {
+                current--;
+                listImg.style.transform = `translateX(${-w * current}px)`;
+            }
+            clear = setInterval(() => { changeImg()}, 2500); 
+        }
+        next.onclick = function() {
+            clearInterval(clear);
+            changeImg();
+            clear = setInterval(() => { changeImg()}, 2500);
+        }
 }    
